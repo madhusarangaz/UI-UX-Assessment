1f2c75744a271c1c31718681323cf61eb1dc1f7d
Kasun Madhusaranga <46866329+madhusarangaz@users.noreply.github.com>
2024-09-17 06:56:38 +0530

    Add files via upload

---
+++ resources/js/app.js
@@ -0,0 +1,19 @@
+// $('.slider-2').on('init', function(event, slick){
+//     slideCount = slick.slideCount;
+//     setSlideCount();
+//     setCurrentSlideNumber(slick.currentSlide);
+//   });
+  
+//   $('.slider-2').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+//     setCurrentSlideNumber(nextSlide);
+//   });
+  
+//   function setSlideCount() {
+//     var $el = $('.slide-count-wrap').find('.total');
+//     $el.text(slideCount);
+//   }
+  
+//   function setCurrentSlideNumber(currentSlide) {
+//     var $el = $('.slide-count-wrap').find('.current');
+//     $el.text(currentSlide + 1);
+//   }
